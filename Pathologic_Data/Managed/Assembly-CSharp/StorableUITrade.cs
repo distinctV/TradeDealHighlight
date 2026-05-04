cc2149af67ac0ba045030ea30446d2fb7c451cf2
distinctV <devanGconrad@gmail.com>
2026-05-03 23:18:18 -0400

    error colors

--- Pathologic_Data/Managed/Assembly-CSharp/StorableUITrade.cs
+++ Pathologic_Data/Managed/Assembly-CSharp/StorableUITrade.cs
@@ -1,92 +1,177 @@
 using System;
+using Engine.Common.Components;
 using Engine.Impl.UI.Menu.Protagonist.Inventory;
 using Engine.Source.Components;
 using Engine.Source.UI.Menu.Protagonist.Inventory;
 using InputServices;
+using PLVirtualMachine.Common.EngineAPI.VMECS;
 using UnityEngine;
 
-public class StorableUITrade : Engine.Impl.UI.Menu.Protagonist.Inventory.StorableUI
+// Token: 0x0200011A RID: 282
+public class StorableUITrade : StorableUI
 {
-  [SerializeField]
-  private GameObject _selectedImage;
-  private int selectedCount;
+	// Token: 0x06000694 RID: 1684
+	private void Start()
+	{
+		this._selectedImage.SetActive(false);
+	}
 
-  private void Start() => _selectedImage.SetActive(false);
+	// Token: 0x06000695 RID: 1685
+	protected override void Update()
+	{
+		if (this.internalStorable.Max > 1)
+		{
+			this.textCount.text = ((this.selectedCount == 0) ? this.internalStorable.Count.ToString() : (this.selectedCount.ToString() + "/" + this.internalStorable.Count.ToString()));
+			if (this.textCount.gameObject != null)
+			{
+				this.textCount.gameObject.SetActive(true);
+			}
+		}
+		else
+		{
+			this.textCount.text = null;
+			if (this.textCount.gameObject != null)
+			{
+				this.textCount.gameObject.SetActive(false);
+			}
+		}
+		this.selectedImage.gameObject.SetActive(this.isSelected);
+		Color color = this.enabledBackgroundColor;
+		if (!this.isEnabled)
+		{
+			color = this.disabledBackgroundColor;
+		}
+		else
+		{
+			int debugState = 0;
+			IStorableComponent internalStorable = this.internalStorable;
+			bool flag;
+			if (internalStorable == null)
+			{
+				flag = null != null;
+			}
+			else
+			{
+				IStorageComponent storage = internalStorable.Storage;
+				flag = ((storage != null) ? storage.Owner : null) != null;
+			}
+			if (flag)
+			{
+				bool playerBuying = this.internalStorable.Storage.Owner.GetComponent<PlayerControllerComponent>() == null;
+				float basePrice = 0f;
+				if (VMGlobalMarketManager.Instance != null)
+				{
+					basePrice = VMGlobalMarketManager.Instance.GetCurrentItemTradeGlobalPrice(this.internalStorable.Owner.Name, !playerBuying);
+				}
+				else
+				{
+					debugState = 7;
+				}
+				if (debugState == 0 && basePrice < 0.0001f)
+				{
+					debugState = 6;
+				}
+				float actualPrice = (playerBuying ? this.internalStorable.Invoice.SellPrice : this.internalStorable.Invoice.BuyPrice);
+				if (debugState == 0 && Mathf.Abs(actualPrice - basePrice) > 0.01f)
+				{
+					if (playerBuying)
+					{
+						if (actualPrice < basePrice)
+						{
+							debugState = 1;
+						}
+						else
+						{
+							debugState = 2;
+						}
+					}
+					else if (actualPrice > basePrice)
+					{
+						debugState = 3;
+					}
+					else
+					{
+						debugState = 4;
+					}
+				}
+			}
+			else
+			{
+				debugState = 5;
+			}
+			switch (debugState)
+			{
+			case 1:
+				color = new Color(0.3f, 1f, 0.3f, 0.3f);
+				goto IL_02D9;
+			case 2:
+				color = new Color(1f, 0.3f, 0.3f, 0.3f);
+				goto IL_02D9;
+			case 3:
+				color = new Color(0.7f, 1f, 0.3f, 0.3f);
+				goto IL_02D9;
+			case 4:
+				color = new Color(1f, 0.5f, 0.3f, 0.3f);
+				goto IL_02D9;
+			case 5:
+				color = new Color(0.5f, 0.5f, 0.5f, 0.3f);
+				goto IL_02D9;
+			case 6:
+				color = new Color(0f, 1f, 1f, 0.3f);
+				goto IL_02D9;
+			case 7:
+				color = new Color(1f, 0f, 1f, 0.3f);
+				goto IL_02D9;
+			}
+			color = new Color(1f, 1f, 0.7f, 0.3f);
+		}
+		IL_02D9:
+		base.ImageBackground.color = color;
+	}
 
-  protected override void Update()
-  {
-    if (internalStorable.Max > 1)
-    {
-      textCount.text = selectedCount == 0 ? internalStorable.Count.ToString() : selectedCount + "/" + internalStorable.Count;
-      if (textCount.gameObject != null)
-        textCount.gameObject.SetActive(true);
-    }
-    else
-    {
-      textCount.text = null;
-      if (textCount.gameObject != null)
-        textCount.gameObject.SetActive(false);
-    }
-    selectedImage.gameObject.SetActive(isSelected);
-    Color color = enabledBackgroundColor;
-    if (!isEnabled)
-    {
-      color = disabledBackgroundColor;
-    }
-    else
-    {
-      int currentDealStatus = 0;
-      if (internalStorable?.Storage?.Owner != null && PLVirtualMachine.Common.EngineAPI.VMECS.VMGlobalMarketManager.Instance != null)
-      {
-         bool playerBuying = internalStorable.Storage.Owner.GetComponent<Engine.Source.Components.PlayerControllerComponent>() == null;
-         float basePrice = PLVirtualMachine.Common.EngineAPI.VMECS.VMGlobalMarketManager.Instance.GetCurrentItemTradeGlobalPrice(internalStorable.Owner.Name, !playerBuying);
-         float actualPrice = playerBuying ? internalStorable.Invoice.SellPrice : internalStorable.Invoice.BuyPrice;
+	// Token: 0x06000696 RID: 1686
+	protected override void CalculatePosition()
+	{
+		if (this.internalStorable == null || this.internalStorable.IsDisposed)
+		{
+			return;
+		}
+		base.CalculatePosition();
+		Vector2 vector2 = ((!this.cellStyle.IsSlot) ? InventoryUtility.CalculateInnerSize(((StorableComponent)this.internalStorable).Placeholder.Grid, this.cellStyle) : InventoryUtility.CalculateInnerSize(StorableUI.gridSlot, this.cellStyle));
+		this.selectedImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector2.x + this.cellStyle.BackgroundImageOffset.x * 2f);
+		this.selectedImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, vector2.y + this.cellStyle.BackgroundImageOffset.x * 2f);
+	}
 
-         if (basePrice > 0.0001f && Mathf.Abs(actualPrice - basePrice) > 0.01f)
-         {
-            if (playerBuying)
-                currentDealStatus = actualPrice < basePrice ? 1 : -1;
-            else
-                currentDealStatus = actualPrice > basePrice ? 1 : -1;
-         }
-      }
+	// Token: 0x06000697 RID: 1687
+	public int GetSelectedCount()
+	{
+		return this.selectedCount;
+	}
 
-      if (currentDealStatus == 1)
-          color = new Color(0.7f, 1f, 0.7f, 0.3f);
-      else if (currentDealStatus == -1)
-          color = new Color(1f, 0.7f, 0.7f, 0.3f);
-      else
-          color = new Color(1f, 1f, 0.7f, 0.3f);
-    }
-    ImageBackground.color = color;
-  }
-  
-  protected override void CalculatePosition()
-  {
-    if (internalStorable == null || internalStorable.IsDisposed)
-      return;
-    base.CalculatePosition();
-    Vector2 vector2 = !cellStyle.IsSlot ? InventoryUtility.CalculateInnerSize(((StorableComponent) internalStorable).Placeholder.Grid, cellStyle) : InventoryUtility.CalculateInnerSize(gridSlot, cellStyle);
-    selectedImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, vector2.x + cellStyle.BackgroundImageOffset.x * 2f);
-    selectedImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, vector2.y + cellStyle.BackgroundImageOffset.x * 2f);
-  }
+	// Token: 0x06000698 RID: 1688
+	public void SetSelectedCount(int count, bool isInit = false)
+	{
+		this.selectedCount = count;
+		if (InputService.Instance.JoystickUsed)
+		{
+			this._selectedImage.SetActive(count > 0);
+			if (isInit)
+			{
+				this.isSelected = false;
+			}
+		}
+		else
+		{
+			this.isSelected = count > 0;
+			this._selectedImage.SetActive(false);
+		}
+		this.Update();
+	}
 
-  public int GetSelectedCount() => selectedCount;
+	// Token: 0x04000602 RID: 1538
+	[SerializeField]
+	private GameObject _selectedImage;
 
-  public void SetSelectedCount(int count, bool isInit = false)
-  {
-    selectedCount = count;
-    if (InputService.Instance.JoystickUsed)
-    {
-      _selectedImage.SetActive(count > 0);
-      if (isInit)
-        isSelected = false;
-    }
-    else
-    {
-      isSelected = count > 0;
-      _selectedImage.SetActive(false);
-    }
-    Update();
-  }
+	// Token: 0x04000603 RID: 1539
+	private int selectedCount;
 }
